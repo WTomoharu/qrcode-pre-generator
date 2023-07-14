6d6859b420f267bdf12068c957a2994b9d138ac5
Tomoharu Watanabe <tomoharu.dev@gmail.com>
2023-07-14 22:45:58 +0900

    不要なものを削除し整理

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+
+import './main.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <h1>Hello World</h1>
   </React.StrictMode>,
 )
