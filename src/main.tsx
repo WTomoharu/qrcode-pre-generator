7f764ac51072fc0705cb6b540138d980eab0f7a8
Tomoharu Watanabe <tomoharu.dev@gmail.com>
2023-07-15 02:31:05 +0900

    chakra uiとemotionを導入

--- src/main.tsx
+++ src/main.tsx
@@ -2,17 +2,20 @@ import React, { Suspense } from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router, useRoutes } from 'react-router-dom'
 
+import { ChakraProvider } from '@chakra-ui/react'
+
 import routes from '~react-pages'
 
-import './main.css'
-
 import "./lib/firebase"
+import './main.css'
 
 const App = () => {
   return (
-    <Suspense fallback={<p>Loading...</p>}>
-      {useRoutes(routes)}
-    </Suspense>
+    <ChakraProvider>
+      <Suspense fallback={<p>Loading...</p>}>
+        {useRoutes(routes)}
+      </Suspense>
+    </ChakraProvider>
   )
 }
 
