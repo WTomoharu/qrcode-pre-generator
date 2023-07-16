e673469b9f4badc43a96c7ca737785c53701a390
Tomoharu Watanabe <tomoharu.dev@gmail.com>
2023-07-16 17:29:22 +0900

    シート作成を実装

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,20 +1,56 @@
+import { Button } from "@chakra-ui/react"
 import { signOut } from "firebase/auth"
 import { FirebaseAuthProtector } from "../../hook/firebase-auth-protector"
 import { auth } from "../../lib/firebase"
+import { collection } from "firebase/firestore"
+import { addDocSafely, firestore } from "../../lib/firestore"
+import { range } from "../../lib/utils"
+import { useNavigate } from "react-router-dom"
+import { ButtonWithLoading } from "../../component/button-with-loading"
+
+async function logout() {
+  return signOut(auth).catch(console.error)
+}
+
+async function createSheet(uid: string) {
+  const sheet = await addDocSafely(collection(firestore, "/sheets"), { 
+    uid,
+  })
+
+  const qrcodes = await Promise.all(range(5).map(() => {
+    return addDocSafely(collection(firestore, "/qrcodes"), { 
+      uid,
+      sheet: sheet.id,
+      url: null
+    })
+  }))
+
+  console.log("sheet: ", sheet)
+  console.log("qrcodes: ", qrcodes.map(qrcode => qrcode.id).join(", "))
+
+  return sheet.id
+}
 
 export const Page = () => {
-  const onClickLogoutButton = () => {
-    signOut(auth)
-  }
+  const navigate = useNavigate()
 
   return (
     <>
       <FirebaseAuthProtector>
         {(user) => (
             <>
-              <h1>Admin</h1>
-              <h1>{user.email}</h1>
-              <button onClick={onClickLogoutButton}>logout</button><br />  
+              <h1>
+                管理者ページ ({user.email})
+              </h1>
+              <div>
+                <Button onClick={logout}>ログアウト</Button>
+              </div>
+              <div>
+                <ButtonWithLoading onClick={async () => {
+                  const sheet = await createSheet(user.uid)
+                  navigate(`/admin/sheet?id=${sheet}`)
+                }}>QRコードシートを作成</ButtonWithLoading>
+              </div>
             </>
           )}
       </FirebaseAuthProtector>
