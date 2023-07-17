35e354efc1acd1e5c00afa8cd4860fb9594bb66e
Tomoharu Watanabe <tomoharu.dev@gmail.com>
2023-07-18 00:37:24 +0900

    index.tsにqrcodeのリダイレクトを実装

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,74 @@
-export const Page = () => {
+import { Button, Text } from "@chakra-ui/react"
+import { doc, getDoc } from "firebase/firestore"
+import { useNavigate, useSearchParams } from "react-router-dom"
+import useSWR from "swr"
+import { CommonStaticModal } from "../component/common-modal"
+import { firestore } from "../lib/firestore"
+
+export const QRCodeRedirectPage = (props: { id: string }) => {
+  const navigate = useNavigate()
+
+  const qrcodeRef = doc(firestore, "qrcodes", props.id)
+
+  const { data: qrcodeSnapshot } = useSWR(qrcodeRef.path, () => {
+    return getDoc(qrcodeRef)
+  }, { suspense: true })
+
+  const qrcodeData = qrcodeSnapshot.data()
+
+  if (!qrcodeData) {
+    return (
+      <CommonStaticModal title="エラー">
+        <Text mb="4">
+          指定されたQRコードは存在しません。
+        </Text>
+        <Button
+          onClick={() => {
+            navigate("/")
+          }}
+          width="full"
+        >
+          トップページに戻る
+        </Button>
+      </CommonStaticModal>
+    )
+  } else if (!qrcodeData.url) {
+    return (
+      <CommonStaticModal title="エラー">
+        <Text mb="4">
+          このQRコードには、リンク先が設定されていません。
+        </Text>
+        <Button
+          onClick={() => {
+            navigate("/admin/qrcode?id=" + props.id)
+          }}
+          width="full"
+        >
+          リンクを設定する
+        </Button>
+      </CommonStaticModal>
+    )
+  } else {
+    location.href = qrcodeData.url
+    return null
+  }
+}
+export const IndexPage = () => {
+  const [params] = useSearchParams()
+  const id = params.get("id")
+
   return (
     <>
-      <h1>Hello World</h1>
+      {id ? (
+        <QRCodeRedirectPage id={id} />
+      ) : (
+        <>
+          <h1>Qrcode Pre-Generator</h1>
+        </>
+      )}
     </>
   )
 
 }
 
-export default Page
+export default IndexPage
