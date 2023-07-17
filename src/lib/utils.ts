4ddcdbc164c882d96db0089b440c9e6b31686d09
Tomoharu Watanabe <tomoharu.dev@gmail.com>
2023-07-18 06:26:48 +0900

    sleep, minTimeを実装

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,17 @@
 export function range(length: number) {
   return Array.from({ length }, (_, i) => i)
 }
+
+export async function sleep(ms: number) {
+  return new Promise<void>(resolve => setTimeout(resolve, ms))
+}
+
+export async function minTime<T extends unknown>(promise: Promise<T>, ms: number): Promise<T> {
+  return Promise.allSettled([promise, sleep(ms)]).then(async ([result, _]) => {
+    if (result.status === "fulfilled") {
+      return result.value  
+    } else {
+      throw result.reason
+    }
+  })
+}
